346ba480de81ff06883531996ffc0ef8e813094b
Andrea Debernardi <andrea@debbaweb.it>
2025-01-10 15:38:49 +0100

    account title fix

--- admin/partials/emailchef-page-forms-display.php
+++ admin/partials/emailchef-page-forms-display.php
@@ -10,7 +10,7 @@
             <div><strong><?php _e("Account connected", "emailchef"); ?></strong></div>
         </div>
         <div class="ecf-account-info">
-            <span class="flex-grow-1 truncate" title="alessandro@sendblaster.com"><?php echo $account->email; ?></span>
+            <span class="flex-grow-1 truncate" title="<?php echo $account->email; ?>"><?php echo $account->email; ?></span>
             <span>
                 <a id="emailchef-disconnect" class="ecf-account-disconnect" title="<?php _e("Disconnect account", "emailchef"); ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M280 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 240c0 13.3 10.7 24 24 24s24-10.7 24-24l0-240zM134.2 107.3c10.7-7.9 12.9-22.9 5.1-33.6s-22.9-12.9-33.6-5.1C46.5 112.3 8 182.7 8 262C8 394.6 115.5 502 248 502s240-107.5 240-240c0-79.3-38.5-149.7-97.8-193.3c-10.7-7.9-25.7-5.6-33.6 5.1s-5.6 25.7 5.1 33.6c47.5 35 78.2 91.2 78.2 154.7c0 106-86 192-192 192S56 368 56 262c0-63.4 30.7-119.7 78.2-154.7z"></path></svg>
